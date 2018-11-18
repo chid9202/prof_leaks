2de26f2c9e7502cfb765af4cad92a3fdfffd576e
Daehan <chid9202@gmail.com>
2018-11-17 19:24:30 -0800

    commit test

--- professor_leaks/pl_app/urls.py
+++ professor_leaks/pl_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('school_list/<str:search_school>', views.school_list, name=school_list),
+    # path('schoo_detail/<str:school_name>', views.school_detail, name=school_detail),
+
 ]
