8bb059cfb052ca7bcb0d7613709a49f5e8d2355a
Daehan <chid9202@gmail.com>
2018-11-17 19:57:56 -0800

    add structure on templates

--- professor_leaks/pl_app/urls.py
+++ professor_leaks/pl_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('school_list/<str:search_school>', views.school_list, name=school_list),
-    # path('schoo_detail/<str:school_name>', views.school_detail, name=school_detail),
-
+    path('school_list/<str:search_school>', views.school_list, name='school_list'),
+    path('school_detail/<str:search_school>', views.school_detail, name='school_detail'),
 ]
--- professor_leaks/pl_app/views.py
+++ professor_leaks/pl_app/views.py
@@ -6,3 +6,17 @@ def index(request):
     return render( request, 'index.html', {
         'name': "daehan"
     })
+
+
+def school_list(request, search_school):
+    # return HttpResponse("List page with %s." % search_school)
+
+    return render( request, 'school_list.html', {
+        'search_school': search_school,
+    })
+
+def school_detail(request, search_school):
+    return render( request, 'school_detail.html', {
+        'name': "daehan",
+        'search_school': search_school,
+    })
